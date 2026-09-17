592a95072cb002c0c42861ddb3db88d23ed46af4
dev-akshatshukla <akshat21shukla@gmail.com>
2026-09-17 18:40:05 +0530

    Initial commit: set up cli-radio project structure

---
+++ index.js
@@ -0,0 +1,6 @@
+#!/usr/bin/env node
+
+const stations = require('./stations');
+const player = require('./player');
+
+console.log('cli-radio');
---
+++ stations.js
@@ -0,0 +1,3 @@
+const stations = [];
+
+module.exports = stations;
