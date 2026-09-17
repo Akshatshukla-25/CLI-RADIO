0cde42c1c8e85e430d937f6adb7712acb530d886
dev-akshatshukla <akshat21shukla@gmail.com>
2026-09-17 18:42:18 +0530

    Add placeholder radio stations to stations.js

--- stations.js
+++ stations.js
@@ -1,3 +1,19 @@
-const stations = [];
+const stations = [
+  {
+    name: 'Placeholder Station 1',
+    genre: 'Lo-Fi',
+    streamUrl: 'https://example.com/stream1.mp3',
+  },
+  {
+    name: 'Placeholder Station 2',
+    genre: 'Jazz',
+    streamUrl: 'https://example.com/stream2.mp3',
+  },
+  {
+    name: 'Placeholder Station 3',
+    genre: 'Classical',
+    streamUrl: 'https://example.com/stream3.mp3',
+  },
+];
 
 module.exports = stations;
