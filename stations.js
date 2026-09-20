b9d39c056f02741f311788dad6ad5fec3e6d0b0b
dev-akshatshukla <akshat21shukla@gmail.com>
2026-09-20 21:39:25 +0530

    feat(stations): add verified SomaFM internet radio stations

--- stations.js
+++ stations.js
@@ -1,19 +1,37 @@
-const stations = [
+module.exports = [
   {
-    name: 'Placeholder Station 1',
-    genre: 'Lo-Fi',
-    streamUrl: 'https://example.com/stream1.mp3',
+    name: 'SomaFM Groove Salad',
+    genre: 'Ambient / Downtempo',
+    streamUrl: 'https://ice.somafm.com/groovesalad',
   },
   {
-    name: 'Placeholder Station 2',
-    genre: 'Jazz',
-    streamUrl: 'https://example.com/stream2.mp3',
+    name: 'SomaFM Drone Zone',
+    genre: 'Ambient',
+    streamUrl: 'https://ice.somafm.com/dronezone',
   },
   {
-    name: 'Placeholder Station 3',
-    genre: 'Classical',
-    streamUrl: 'https://example.com/stream3.mp3',
+    name: 'SomaFM Indie Pop Rocks!',
+    genre: 'Indie Pop',
+    streamUrl: 'https://ice.somafm.com/indiepop',
+  },
+  {
+    name: 'SomaFM Deep Space One',
+    genre: 'Ambient / Electronic',
+    streamUrl: 'https://ice.somafm.com/deepspaceone',
+  },
+  {
+    name: 'SomaFM Lush',
+    genre: 'Electronic / Vocal',
+    streamUrl: 'https://ice.somafm.com/lush',
+  },
+  {
+    name: 'SomaFM Space Station Soma',
+    genre: 'Ambient / Electronica',
+    streamUrl: 'https://ice.somafm.com/spacestation',
+  },
+  {
+    name: 'SomaFM Groove Salad Classic',
+    genre: 'Ambient / Downtempo (Classic)',
+    streamUrl: 'https://ice.somafm.com/gsclassic',
   },
 ];
-
-module.exports = stations;
